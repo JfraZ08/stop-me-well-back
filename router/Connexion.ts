39fb00fc50776c0b1cfa0796d407fb1717ad50e4
Jeremy FRAZIER <frazierjeremi@gmail.com>
2024-01-16 11:28:19 +0100

    test connexion routeur fail

---
+++ router/Connexion.ts
@@ -0,0 +1,40 @@
+import { Router } from 'express';
+import { showConnexion } from '..';
+import { compare } from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+export const ConnexionRouter = Router();
+
+ConnexionRouter.post('http://localhost:3012/auth/connect', async (req, res) => {
+    const findUser = await User.findOne( { where : { email : req.body.login }})
+    if(!findUser){
+        res.status(400).json({ error : "reponse"})
+    }
+    else {
+        const isSamePassword = await compare(req.body.password, findUser.dataValues.password)
+        if(isSamePassword){
+            const token = jwt.sign({userId: findUser.dataValues.id}, 'exact')
+            res.json({
+                findUser,
+                token
+            });
+        }else {
+           res.status(400).json({error : "le mot de passe n'est pas le bon "})
+        }
+    }
+})
+
+ConnexionRouter.post('http://localhost:3012/auth/connect', async (req, res) => {
+    console.log('res', req.body)
+    const findUser = await User.findOne( { where : { email : req.body.identifier }})
+    if(findUser){
+        res.status(400).json('utilisateur existe déjà')
+    }else{
+
+        const newUser = await User.create({
+            email : req.body.identifier,
+            password : req.body.password
+        })
+        res.json(newUser);
+    }
+})
