57fcb4b492cb9b0df408335992a36bb98583e60b
Jeremy FRAZIER <frazierjeremi@gmail.com>
2024-01-16 10:11:12 +0100

    players

--- index.ts
+++ index.ts
@@ -5,3 +5,12 @@ import { Server } from "socket.io";
 const app = express();
 const server = createServer(app);
 const io = new Server(server);
+let Players: any[] = [];
+let nbTurns = 0;
+
+io.on('connection', (socket) => {
+    Players.push(socket)
+    if(Players.length===2){
+        console.log('partieprete')
+    }
+})
