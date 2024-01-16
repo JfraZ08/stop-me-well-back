967e90278beca2539465c2c41448ec83b726c980
Jeremy FRAZIER <frazierjeremi@gmail.com>
2024-01-16 09:49:53 +0100

    debut serveur

--- index.ts
+++ index.ts
@@ -1,2 +1,7 @@
-import { Express } from "express";
+import express from "express";
 import { createServer } from "node:http";
+import { Server } from "socket.io";
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
