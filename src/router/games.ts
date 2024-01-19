b2e98ec3ee6021dff2ea0e64ddca81f0bd488493
Jeremy FRAZIER <frazierjeremi@gmail.com>
2024-01-19 16:37:03 +0100

    first part games.ts

--- src/router/games.ts
+++ src/router/games.ts
@@ -1,5 +1,18 @@
 import { Router } from "express";
-import { gamesRoutes } from "..";
+import { Game, User } from "..";
+import { middleware } from "../middlewares/checkToken";
 
 
+export const gamesRoutes = Router();
 
+gamesRoutes.get("/",middleware,async (req, res) => {
+    const game = await Game.findAll();
+    const user = await User.findOne({ where: { id: req.userId } });
+    if (user) {
+        res.status(200).json({
+            data:game
+        });
+    }else {
+        res.status(404).json({ error: "Merci de vous connecter" });
+    }
+});
