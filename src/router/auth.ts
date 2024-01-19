420418a8007a8c2c4de7f7f49f6974c88d7eeda6
Jeremy FRAZIER <frazierjeremi@gmail.com>
2024-01-19 16:21:25 +0100

    route connect ok

--- src/router/auth.ts
+++ src/router/auth.ts
@@ -15,7 +15,15 @@ authRoutes.post('/connect'), async (req, res: any) => {
         if(match){
             const dataToSign: IJwtInfo = { userId: log.dataValues.id };
             const token = jwt.sign(dataToSign, process.env.JWT_SECRET!);
+            res.json({
+                jwtToken : token
+            });
         }
+        else {
+            res.status(400).json({ error: "indentifiants incorrects"});
+        }
+    } else {
+        res.status(400).json({ error : "Cet utilisateur n'existe pas"});
     }
 
-}
+});
